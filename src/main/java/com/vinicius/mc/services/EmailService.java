48e8a51ff398b08952092bf8441a5c32defe0243
Vinicius Giacomelli <giacomelli.vinicius@gmail.com>
2020-09-08 09:58:40 -0300

    email formatado com Html

--- src/main/java/com/vinicius/mc/services/EmailService.java
+++ src/main/java/com/vinicius/mc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.vinicius.mc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.vinicius.mc.domain.Pedido;
@@ -9,5 +11,11 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
+	
+	
 	
 }
