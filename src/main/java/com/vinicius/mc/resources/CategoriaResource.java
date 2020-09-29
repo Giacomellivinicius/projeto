a5c8e538071bc3d1b11048f70f15d5b6de1f9140
Vinicius Giacomelli <giacomelli.vinicius@gmail.com>
2020-09-29 09:01:53 -0300

    Autorizando endpoints para perfis especificos

--- src/main/java/com/vinicius/mc/resources/CategoriaResource.java
+++ src/main/java/com/vinicius/mc/resources/CategoriaResource.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,6 +43,7 @@ public class CategoriaResource {
 	}
 	
 	//Método INSERT -> insere categoria através do metodo POST
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping( method = RequestMethod.POST)
 	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO objDto){
 		Categoria obj = service.fromDto(objDto);
@@ -54,6 +56,7 @@ public class CategoriaResource {
 	
 	
 	//Método UPDATE -> altera o nome de uma categoria já existente
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value="/{id}",method=RequestMethod.PUT)
 	public ResponseEntity<Void> update(@Valid @RequestBody CategoriaDTO objDto, @PathVariable Integer id){
 		Categoria obj = service.fromDto(objDto);
@@ -65,6 +68,7 @@ public class CategoriaResource {
 	//Método DELETE -> deleta categoria através do Id
 	/*Tratamento de erros por delecão de categoria com produtos
 	  associados adicionada*/
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value="/{id}",method=RequestMethod.DELETE)
 	public ResponseEntity<Void>delete(@PathVariable Integer id){
 		service.delete(id);
