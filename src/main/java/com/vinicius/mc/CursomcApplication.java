4e002c35d74e7f8af46fcc6bb2ba3616da76267a
Vinicius Giacomelli <giacomelli.vinicius@gmail.com>
2020-10-05 17:11:38 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/vinicius/mc/CursomcApplication.java
+++ src/main/java/com/vinicius/mc/CursomcApplication.java
@@ -1,13 +1,17 @@
 package com.vinicius.mc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.vinicius.mc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
-	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -17,7 +21,7 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Override 
 	public void run(String... args)throws Exception{
-		
+		s3Service.uploadFile("C:\\Users\\Vini\\Documents\\wallpaper\\joker.jpg");
 	}
 	
 	
