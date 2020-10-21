ad5c817872e482a514e8534f854e13ec2ec5fb1c
Vinicius Giacomelli <giacomelli.vinicius@gmail.com>
2020-10-21 14:50:28 -0300

    Atualizacao JWTAuthenticationFilter

--- src/main/java/com/vinicius/mc/config/SecurityConfig.java
+++ src/main/java/com/vinicius/mc/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.vinicius.mc.config;
 
 
 import java.util.Arrays;
+import java.util.Collections;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -19,6 +20,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import com.vinicius.mc.security.JWTAuthenticationFilter;
 import com.vinicius.mc.security.JWTAuthorizationFilter;
@@ -89,5 +91,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+	
 	
 }
